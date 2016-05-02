0c5949bab096827584cf777c62b07340c8b1be0c
JeGa <jega@win>
2016-05-02 23:55:54 +0200

    Added background and foreground diff.

--- em.py
+++ em.py
@@ -9,7 +9,7 @@ def getbox(x1, y1, x2, y2, image):
     return image[y1:y2, x1:x2]
 
 
-def em():
+def both():
     # Number of Gaussians
     K = 5
 
@@ -23,9 +23,83 @@ def em():
     print("Image shape:")
     print(img.shape)
 
-    # 480, 365
-    box = getbox(200, 285, 240, 300, img)
+    # Foreground =====================================
+    box = getbox(200, 285, 300, 330, img)
+    # box = getbox(200, 285, 480, 365, img)
 
+    mu_f, sigma_f, mix_f = em_try(box, K, D, 20)
+
+    # Background =====================================
+    box = getbox(0, 0, 50, 50, img)
+    # box = getbox(200, 285, 480, 365, img)
+
+    mu_b, sigma_b, mix_b = em_try(box, K, D, 20)
+
+    ysize, xsize, _ = img.shape
+
+    # xv, yv = np.meshgrid(range(ysize), range(xsize))
+    # pixelCoords = np.dstack((xv, yv))
+    # pixelCoords = pixelCoords.reshape(-1, 2)
+
+    # numPixel = self.xsize * self.ysize
+    # X = np.zeros(
+    #     numPixel, dtype=[('y', 'uint16'), ('x', 'uint16'), ('values', 'float64', (numberOfSamples,))])
+    # X['y'] = pixelCoords[:, 0]
+    # X['x'] = pixelCoords[:, 1]
+
+    gauss_f = []
+    gauss_b = []
+    for k in range(K):
+        gauss_f.append(scipy.stats.multivariate_normal(mu_f[k], sigma_f[k]))
+        gauss_b.append(scipy.stats.multivariate_normal(mu_b[k], sigma_b[k]))
+
+    for y in range(ysize):
+        for x in range(xsize):
+            prob_f = 0
+            prob_b = 0
+            for k in range(K):
+                prob_f += mix_f[k] * gauss_f[k].pdf(img[y, x])
+                prob_b += mix_f[k] * gauss_b[k].pdf(img[y, x])
+
+            if prob_f > prob_b:
+                img[y, x] = np.array([255, 255, 0])
+            else:
+                img[y, x] = np.array([0, 0, 0])
+
+    img = img.astype(np.uint8)
+    plt.imshow(img)
+    plt.show()
+    np.save("imgdata", img)
+
+    pass
+
+
+def test(img, K):
+    ysize, xsize, _ = img.shape
+
+    xv, yv = np.meshgrid(range(ysize), range(xsize))
+    pixelCoords = np.dstack((xv, yv))
+    pixelCoords = pixelCoords.reshape(-1, 2)
+
+    numPixel = xsize * ysize
+    X = np.zeros(numPixel, dtype=[('y', 'uint16'), ('x', 'uint16'), ('prob', 'float64', (K,))])
+    X['y'] = pixelCoords[:, 0]
+    X['x'] = pixelCoords[:, 1]
+    pass
+
+
+def em_try(box, K, D, iterations):
+    while True:
+        try:
+            mu, sigma, mix = em(box, K, D, iterations)
+        except:
+            print("Try ...")
+            continue
+        break
+    return mu, sigma, mix
+
+
+def em(box, K, D, iterations):
     print("Box shape:")
     print(box.shape)
 
@@ -65,7 +139,7 @@ def em():
     print(sigma)
     print(mix)
 
-    for it in range(20):
+    for it in range(iterations):
 
         # E-Step
 
@@ -84,7 +158,7 @@ def em():
         for k in range(K):
             for n in range(N):
                 mu_new[k] += resp[k, n] * X[n]
-            mu_new[k] = mu_new[k,:] / resp[k].sum(0)
+            mu_new[k] = mu_new[k, :] / resp[k].sum(0)
 
         # sigma
         sigma_new = np.empty((K, D, D))
@@ -92,7 +166,7 @@ def em():
             for n in range(N):
                 diff = X[n] - mu_new[k]
                 sigma_new[k] += resp[k, n] * np.outer(diff, diff)
-            sigma_new[k] = sigma_new[k,:] / resp[k].sum(0)
+            sigma_new[k] = sigma_new[k, :] / resp[k].sum(0)
 
         # mix
         mix_new = np.empty(K)
@@ -125,46 +199,11 @@ def em():
     print("Mix")
     print(mix)
 
-    pix = img[220, 290]
-
-    ysize, xsize, _ = img.shape
-
-    # xv, yv = np.meshgrid(range(ysize), range(xsize))
-    # pixelCoords = np.dstack((xv, yv))
-    # pixelCoords = pixelCoords.reshape(-1, 2)
-
-    # numPixel = self.xsize * self.ysize
-    # X = np.zeros(
-    #     numPixel, dtype=[('y', 'uint16'), ('x', 'uint16'), ('values', 'float64', (numberOfSamples,))])
-    # X['y'] = pixelCoords[:, 0]
-    # X['x'] = pixelCoords[:, 1]
-
-    #for k in range(K)
-
-    for y in range(ysize):
-        for x in range(xsize):
-            for k in range(K):
-                prob = scipy.stats.multivariate_normal.pdf(img[y, x], mean=mu[k], cov=sigma[k])
-                if prob >= 1.12140262525e-45:
-                    img[y, x] = np.array([255, 255, 0])
-                    continue
-                else:
-                    img[y, x] = np.array([0, 0, 0])
-
-    plt.imshow(img)
-    plt.show()
-
-    # print(prob)
-    #
-    # pix = img[10, 10]
-    #
-    # for k in range(K):
-    #     prob = scipy.stats.multivariate_normal.pdf(pix, mean=mu[k], cov=sigma[k])
-    #     print(prob)
+    return mu, sigma, mix
 
 
 def main():
-    em()
+    both()
 
 
 if __name__ == '__main__':
